1d3e4f14a6a069d1965d18daebdeb17d3bcd7bcc
Roman <269865002@qq.com>
2023-07-17 17:40:27 +0800

    添加导航栏

--- lib/test/page/test_page.dart
+++ lib/test/page/test_page.dart
@@ -11,8 +11,21 @@ class _Test1PageState extends State<Test1Page> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar( title: Text('导航栏'),),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              ),
+              Positioned(left: 100, right: 10, top: 20, child: Text('这是一个标题')),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
 }
